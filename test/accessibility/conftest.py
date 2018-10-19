f774ea09eb3b41a74d7569d8f9292685743c7649
Kimberly <ksereduck@mozilla.com>
2018-10-19 10:02:30 -0600

    reduced pr to single server test implementing updates to pytest-axe

--- test/accessibility/conftest.py
+++ test/accessibility/conftest.py
@@ -2,19 +2,13 @@ import pytest
 from pytest_axe.pytest_axe import PytestAxe as Axe
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 base_url = "https://screenshots.stage.mozaws.net/"
 
 _panel_button_locator = "pageActionButton"
 _take_screenshot_button_locator = (By.ID, "pageAction-panel-screenshots_mozilla_org")
-_onboarding_iframe_locator = (By.ID, "firefox-screenshots-onboarding-iframe")
-_test_image_locator = "banner-image-front"
-_preselection_iframe_locator = (By.ID, "firefox-screenshots-preselection-iframe")
-_selection_iframe_locator = (By.ID, "firefox-screenshots-selection-iframe")
-_save_screenshots_button_locator = "highlight-button-save"
-_screenshot_page_locator = (By.ID, "react-body-container")
 
 
 def wait_for_element_to_load(driver, selector):
@@ -25,62 +19,19 @@ def wait_for_element_to_load(driver, selector):
 
 @pytest.fixture(scope="class")
 def home_page():
+    """Launch Screenshots home page as guest."""
     profile = webdriver.FirefoxProfile()
+    # Disable CSP so aXe javascript can be injected
     profile.set_preference("security.csp.enable", False)
     driver = webdriver.Firefox(firefox_profile=profile)
     driver.get(base_url)
+
+    # Inject accessibility API into page
     axe = Axe(driver)
     axe.inject()
+
+    # Yield Axe instance, containing the WebDriver object as class attribute
     yield axe
+
+    # Close WebDriver instance
     driver.close()
-
-
-@pytest.fixture(scope="class")
-def onboarding_frame(home_page):
-    driver = home_page.selenium
-    with driver.context("chrome"):
-        btn = driver.find_element_by_id(_panel_button_locator)
-        btn.click()
-        wait_for_element_to_load(driver, _take_screenshot_button_locator).click()
-    onboarding = wait_for_element_to_load(driver, _onboarding_iframe_locator)
-    driver.switch_to.frame(onboarding)
-    axe = Axe(driver)
-    axe.inject()
-    yield axe
-
-
-@pytest.fixture(scope="class")
-def preselection_frame(onboarding_frame):
-    driver = onboarding_frame.selenium
-    driver.find_element_by_id("skip").click()
-    driver.switch_to.default_content()
-    preselection = wait_for_element_to_load(driver, _preselection_iframe_locator)
-    driver.switch_to.frame(preselection)
-    axe = Axe(driver)
-    axe.inject()
-    yield axe
-
-
-@pytest.fixture(scope="class")
-def selection_frame(preselection_frame):
-    driver = preselection_frame.selenium
-    driver.switch_to.default_content()
-    el = driver.find_element_by_class_name(_test_image_locator)
-    action = webdriver.ActionChains(driver).move_to_element(el).click(el)
-    action.perform()
-    selection = wait_for_element_to_load(driver, _selection_iframe_locator)
-    driver.switch_to.frame(selection)
-    axe = Axe(driver)
-    axe.inject()
-    yield axe
-
-
-@pytest.fixture(scope="class")
-def screenshot_page(selection_frame):
-    driver = selection_frame.selenium
-    driver.find_element_by_class_name(_save_screenshots_button_locator).click()
-    driver.switch_to.default_content()
-    wait_for_element_to_load(driver, _screenshot_page_locator)
-    axe = Axe(driver)
-    axe.inject()
-    yield axe
